1432d56653bed708253fc8ce86bda9b4d5aaaa4e
Hardbeir <marcdorac@gmail.com>
2022-09-22 14:50:48 +0200

    finished created router

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
     const [inpval, setINP] = useState({
         name: "",
         subname: "",
-        email: "",
+        email: ""
     })
 
     const setdata = (e) => {
--- server/Routes/router.js
+++ server/Routes/router.js
@@ -4,8 +4,33 @@ const conn = require("./db/conn");
 
 router.post("/create",(req,ser)=>{
 
-    console.log(req.body);
+/*     console.log(req.body); */
+
+const {name, subname, email} = req.body;
+
+if(name || !subname || !email){
+    res.status(422).json("fill the all data")
+}
+
+try{
+    conn.query("SELECT * FROM users WHERE email = ?", email,(err,result)=>{
+        if(result.length){
+            res.status(422).json("Data is Already")
+        } else{
+            conn.query("INSERT INTO users SET ?",{name,subname,email},(err,result)=>{
+                if(err){
+                    console.log("err" + err);
+                }else{
+                    res.status(201).json(req,body);
+                }
+            })
+        }
+    })
+} catch(error){
+    res.status(422).json("fill the all data");
+}
 
 })
 
 module.exports = router;
+
